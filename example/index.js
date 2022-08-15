5a71835f10b882395d14805172ac36031742b7fb
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2022-08-15 20:33:06 +0530

    Example of Input type file added to Example project

--- example/index.js
+++ example/index.js
@@ -6,25 +6,39 @@ const app = new App(document.body)
 app.state = {
     firstname: '',
     lastname: '',
-    // tasks: [1, 2, 3, 4],
+    profile: undefined,
     get fullname() {
         return `${this.firstname} ${this.lastname}`
     },
     get formValid() {
         return this.firstname === "" && this.lastname === ""
-    }
+    },
+    get hideReset() {
+        return this.firstname === "" && this.lastname === ""
+    },
 }
+
 app.queries = [
     { query: 'model', property: 'value', isInput: true },
     { query: 'text', property: 'textContent' },
     { query: 'disable', property: 'disabled' },
+    { query: 'hide', property: 'hidden' }
 ]
 
-document.forms['form'].onreset = (event) => {
-    event.preventDefault()  // override default reset behaviour of form
-    app.resetState()    // will remove all getters
+const profileImage = app.useState('profile', true)
+
+document.forms['form']['profile'].onchange = event => {
+    profileImage.value = event.target.value
+    console.log('profile selected')
 }
-document.forms['form'].onsubmit = (event) => {
+
+document.forms['form'].onreset = event => {
+    app.state.firstname = ''
+    app.state.lastname = ''
+}
+
+document.forms['form'].onsubmit = event => {
     event.preventDefault()  // override default submit behaviour of form 
     console.table(JSON.parse(JSON.stringify(app.state)))
+    console.table(app.elements)
 }
